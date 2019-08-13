cc0d564926f9292ea14c078c40697fc11a9991cb
Eric Loudon <loudon.eric@gmail.com>
2019-08-13 08:38:50 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,5 +4,19 @@ def countdown(count)
   while count > 0
     puts "#{count} SECOND(S)!"
 count -= 1
+end
  "HAPPY NEW YEAR!"
 end
+
+
+def countdown_with_sleep(num)
+
+ while num > 0
+
+   puts "#{num} SECOND(S)!"
+  sleep(1)
+  num -= 1
+
+ end
+  "HAPPY NEW YEAR!"
+end	end
